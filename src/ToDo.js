b238a6531475e4631b024bca0c3da828b1bdd39b
Chayapol Moemeng <mchayapol@gmail.com>
2020-03-25 14:53:59 +0700

    Init

---
+++ src/ToDo.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+function ToDoList(props) {
+  //   const [tasks, setTasks] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/task')
+      .then(res => res.json())
+      .then(data => {
+        // setTasks(res.data);
+        console.log(data);
+        let tasks = data;
+        let L = tasks.map((v, i) => {
+          return <li key={i}>{v.name}</li>;
+        });
+        setList(L);
+      });
+  },[]);
+
+  return (
+    <div>
+      <h2>Things To Do</h2>
+      <ul>{list}</ul>
+    </div>
+  );
+}
+
+export default ToDoList;
